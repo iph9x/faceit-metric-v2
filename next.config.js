92e488021b0ba52a6cca1e00bb6470a45a273bda
iph9x <bezmenovivan@gmail.com>
2022-01-17 17:41:12 +0700

    add env-config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    PUBLIC_URL: "https://iph9x.github.io/faceit-metric-v2",
+    assetPrefix: './'
+  }
+};
